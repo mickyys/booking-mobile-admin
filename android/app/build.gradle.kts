8762f57fd4d718b107ca20e06e46819041589bcd
hmartinez <hamp.martinez@gmail.com>
2026-04-12 00:00:02 -0400

    Update Auth0 manifest placeholders for demo environment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,18 +28,12 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-<<<<<<< HEAD
         manifestPlaceholders.putAll(
             mapOf(
                 "auth0Domain" to "dev-8obo6dl4.us.auth0.com",
                 "auth0Scheme" to "https"
             )
         )
-=======
-
-        manifestPlaceholders["auth0Domain"] = "dev-8obo6dl4.us.auth0.com"
-        manifestPlaceholders["auth0Scheme"] = "demo"
->>>>>>> 03ebf6e3dc618e58397ac029557d805057764fa8
     }
 
     buildTypes {
